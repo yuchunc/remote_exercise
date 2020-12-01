5f1ffafd2d34b66c676a1cfe24f881dd4ca95455
Mickey Chen <poverwhelming@gmail.com>
2020-12-01 18:31:55 +0800

    added User seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,12 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias RemoteExercise.{Repo, Account}
+
+now = NaiveDateTime.utc_now |> NaiveDateTime.truncate(:second)
+users = for _ <- 1..100 do
+  %{point: 0, inserted_at: now, updated_at: now}
+end
+Repo.insert_all(Account.User, users)
+
